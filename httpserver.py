f9830afc8931a17625ed2cd30345474ac6da8fe3
who8736 <who8736@hotmail.com>
2016-11-19 10:54:32 +0800

    20161119, remove unused comment

--- httpserver.py
+++ httpserver.py
@@ -6,11 +6,8 @@ Created on 2016年11月4日
 @author: who8736
 '''
 
-from flask import Flask, request, render_template, url_for
-# import json
-# import traceback
+from flask import Flask, request, render_template
 
-from xp1024.pagemanager import read_page_list
 from dbmanager import read_page_list
 
 
@@ -21,11 +18,7 @@ app.config.from_object('flaskconfig')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    #     my_page_list = {
-    #         "pagetitle": "[11.04] ♠유最新日本騎兵웃♣新片合集♥95℃ [11.04]",
-    #         "pagecode": "469049"}
     my_page_list = read_page_list()
-#     print url_for('static')
     for (k, v) in my_page_list:
         print k, repr(v)
     return render_template('index.html', page_list=my_page_list)
@@ -33,11 +26,7 @@ def home():
 
 @app.route('/index_nav', methods=['GET', 'POST'])
 def home_nav():
-    #     my_page_list = {
-    #         "pagetitle": "[11.04] ♠유最新日本騎兵웃♣新片合集♥95℃ [11.04]",
-    #         "pagecode": "469049"}
     my_page_list = read_page_list()
-#     print url_for('static')
     for (k, v) in my_page_list:
         print k, repr(v)
     return render_template('index_nav.html', page_list=my_page_list)
@@ -60,18 +49,3 @@ def signin():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True)
-
-# from flask import Flask
-# from flask import request, render_template
-#
-# app = Flask(__name__)
-#
-#
-# @app.route('/', methods=['GET', 'POST'])
-# def home():
-#     return render_template('index.html')
-# #     return '<h1>Home</h1>'
-#
-#
-# if __name__ == '__main__':
-#     app.run()
